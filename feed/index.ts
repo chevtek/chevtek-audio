904e444dc1fe3a5101441ef56b0912f92fdb7252
Chev <chev@chevtek.io>
2020-08-05 02:14:14 -0600

    Updated dependencies and moved podcast show details into a storage file share.

--- feed/index.ts
+++ feed/index.ts
@@ -1,19 +1,31 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { BlobServiceClient } from "@azure/storage-blob";
+import { ShareServiceClient } from "@azure/storage-file-share";
 import * as Podcast from "podcast";
-import * as fs from "fs";
-import * as util from "util";
-const readFileAsync = util.promisify(fs.readFile);
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   try {
-    const podcastName = context.bindingData.podcastName;
-    const podcastData = JSON.parse((await readFileAsync(`./feed/${podcastName}.json`)).toString());
+    // Pull podcast name from URL.
+    const { podcastName } = context.bindingData;
+
+    // Pull podcast JSON metadata from azure storage file share.
+    const serviceClient = ShareServiceClient.fromConnectionString(process.env["chevtekpodcasts_STORAGE"]);
+    const fileClient = serviceClient
+      .getShareClient("feeds")
+      .rootDirectoryClient
+      .getFileClient(`${podcastName}.json`);
+    const fileResponse = await fileClient.download();
+    const podcastData = JSON.parse(await streamToString(fileResponse.readableStreamBody));
+
+    // Create the podcast feed.
     const podcastFeed = new Podcast(podcastData);
+
+    // Find all podcast entries from azure blob storage.
     const blobServiceClient = BlobServiceClient.fromConnectionString(process.env["chevtekpodcasts_STORAGE"]);
     const containerClient = blobServiceClient.getContainerClient(podcastName);
+    const blobs = await containerClient.listBlobsFlat({ includeMetadata: true });
 
-    const blobs = await containerClient.listBlobsFlat({ include: ["metadata"] });
+    // Iterate over each blob and read the metadata into a podcast feed entry.
     for await (const blob of blobs) {
       if (blob.metadata.isLive) {
         let episodeUrl = `${process.env.PROTOCOL}://${process.env.ANALYTICS_PREFIX}${process.env.DOMAIN}/episode/${podcastName}/${blob.name}`;
@@ -21,7 +33,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
           title: blob.metadata.title,
           description: blob.metadata.description,
           url: episodeUrl,
-          date: blob.metadata.date || blob.properties.creationTime,
+          date: blob.metadata.date || blob.properties.createdOn,
           enclosure: {
             url: episodeUrl,
             size: blob.properties.contentLength,
@@ -41,6 +53,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
       }
     }
 
+    // Return the response.
     context.res = {
       status: 200,
       headers: {
@@ -53,5 +66,19 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     throw err;
   }
 };
+
+// [Node.js only] A helper method used to read a Node.js readable stream into string
+async function streamToString(readableStream) {
+  return new Promise<string>((resolve, reject) => {
+    const chunks = [];
+    readableStream.on("data", (data) => {
+      chunks.push(data.toString());
+    });
+    readableStream.on("end", () => {
+      resolve(chunks.join(""));
+    });
+    readableStream.on("error", reject);
+  });
+}
 
 export default httpTrigger;
